107b2001487fc1350a062ddfdb08fe56f0d15538
bruceamiller <159710432+bruceamiller@users.noreply.github.com>
2025-03-22 16:57:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ from PIL import Image
 def getImage(image):
     return image
 
-def startCrop():
+def getImageInfo(image):
+    #Change bug type according to yolo...?:
+
+    #Print text if bug type:
+    if (currentBug == None):
+        return ""
+    elif (currentBug == "mosquito"):
+        return "mosquito bites are itchy"
+    else:
+        return "Invalid bug type"
 
 
-finalImage = None
+currentBug = "mosquito"
 
-imageGetter = gr.Image(show_fullscreen_button=False)
-imageCropper = gr.ImageEditor(transforms="crop", interactive=True, eraser=False, brush=False, layers=False, crop_size=)
-inputWindow = gr.Interface(fn=getImage, inputs= imageCropper, outputs=finalImage)
+imageCropper = gr.ImageEditor(transforms="crop", interactive=True, eraser=False, brush=False, layers=False)
+textBox = gr.Textbox(label = "Bug Info:", interactive=False)
 
-imageCropper.input()
+inputWindow = gr.Interface(fn=getImageInfo, inputs= imageCropper, outputs=textBox, live=True)
 
 inputWindow.launch(share=True)
 
