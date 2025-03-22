ed40460bd7f2573b4d361f13434d12e6305970b2
bruceamiller <159710432+bruceamiller@users.noreply.github.com>
2025-03-22 16:12:59 -0500

    image Files

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 #pip install gradio
+#pip install pillow
+
 import gradio as gr
 from PIL import Image
-
-def greet(name):
-    greeting = "Hello " + name
-    return greeting
 
 def getImage(image):
     return image
 
-#demo = gr.Interface(fn= greet,
-#             inputs= gr.Textbox(),
-#             outputs=gr.Textbox(),
-#             )
+def startCrop():
+    
 
 finalImage = None
 
@@ -20,5 +16,7 @@ imageGetter = gr.Image(show_fullscreen_button=False)
 imageCropper = gr.ImageEditor(transforms="crop", interactive=True, eraser=False, brush=False, layers=False)
 inputWindow = gr.Interface(fn=getImage, inputs= imageCropper, outputs=finalImage)
 
-inputWindow.launch()
+imageCropper.input()
+
+inputWindow.launch(share=True)
 
