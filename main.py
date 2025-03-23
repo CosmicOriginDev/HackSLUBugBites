eea0680df8114d727f8250ace886929045cfe1a2
bruceamiller <159710432+bruceamiller@users.noreply.github.com>
2025-03-23 09:35:05 -0500

    Working Submission

--- main.py
+++ main.py
@@ -10,22 +10,30 @@ import Output
 model = YOLO("runs\\detect\\train\\weights\\best.pt")
 currentBug = None
 
-
 def getImageInfo(imageParam):
    
     # Ensure the image is correctly formatted before YOLO
     result = model.predict(source=imageParam, save=True)
     
     setOfBugs = set()
-    if result[0].boxes and result[0].boxes.cls is not None:
-        potentialBugs = result[0].boxes.cls
+    
+    if result[0].boxes and result[0].boxes.cls is not None and imageParam != None:
+        bugNumsTensor = result[0].boxes.cls
         outputString = ""
-        for bugNumber in potentialBugs:
-            bugNumber = int(bugNumber)
-            setOfBugs.add(result[0].names[bugNumber])
-        for bug in setOfBugs:
-            outputString += bug + " : " + Output.bite_info[bug] + "\n"
-            return outputString
+        listOfBugNums= list()
+        setOfBugNums= set()
+        for bugNumber in bugNumsTensor:
+            setOfBugNums.add(int(bugNumber))
+            listOfBugNums.append(int(bugNumber))
+        setOfBugCount = list()
+        setOfBugNames = list()
+        for bugNumber in setOfBugNums:
+            setOfBugCount.append(listOfBugNums.count(bugNumber))
+            setOfBugNames.append(result[0].names[int(bugNumber)])
+        largestBugName = setOfBugNames[setOfBugCount.index(max(setOfBugCount))]
+        print(largestBugName + " : " + Output.bite_info[largestBugName] + "\n")
+        outputString += largestBugName + " : " + Output.bite_info[largestBugName]
+        return outputString
     else:
         return "No bug bite detected."
 
